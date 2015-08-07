efea615d4c3de4c8726f5baca73a19fb52f1d755
Austin Kincaid <aokincaid@gmail.com>
2015-08-07 09:42:45 -0700

    Add spec to handle strings with capitalizatinos

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,6 +1,7 @@
 class String
   define_method(:word_count) do | needle |
-    word_haystack = self.split(' ')
+    word_haystack = self.downcase()
+    word_haystack = word_haystack.split(' ')
     # binding.pry
     return word_haystack.count(needle)
   end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -7,9 +7,13 @@ describe('String#word_count') do
   it('takes a user provided simple string and simple argument string and returns how many times the argument string appears in the original string') do
     expect(('roses').word_count('roses')).to(eq(1))
   end
-  
+
   it('takes a user provided complex string and simple argument string and returns how many times the argument string appears in the first string') do
-    expect(('roses today are not like roses our parent knew growing up').word_count('roses')).to(eq(2))
+    expect(('tulips today are not like tulips our parent knew growing up').word_count('tulips')).to(eq(2))
+  end
+
+  it('handles strings with capitalizations within the search') do
+    expect(("The Rhododendron is the state flower of Washington. As far as a rhododendron goes, they're nice in a rhododendron way.").word_count('rhododendron')).to(eq(3))
   end
 
 end
