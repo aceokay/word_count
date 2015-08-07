ec4f1553279afc45c637b9051498993d9ac9a006
Austin Kincaid <aokincaid@gmail.com>
2015-08-07 09:30:01 -0700

    add spec to verify method's ability to search through a multi-word string and find multiple occuances of a specified string

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -7,5 +7,9 @@ describe('String#word_count') do
   it('takes a user provided simple string and simple argument string and returns how many times the argument string appears in the original string') do
     expect(('roses').word_count('roses')).to(eq(1))
   end
+  
+  it('takes a user provided complex string and simple argument string and returns how many times the argument string appears in the first string') do
+    expect(('roses today are not like roses our parent knew growing up').word_count('roses')).to(eq(2))
+  end
 
 end
