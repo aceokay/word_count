e493bc0df857f086597e7d442805bd96765df3aa
Austin Kincaid <aokincaid@gmail.com>
2015-08-07 10:10:10 -0700

    Add spec and method modification to handle select punctuation through target string

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,13 @@
 class String
   define_method(:word_count) do | needle |
+    needle.downcase!()
+    punctuation_list = [',','.','?','!','(',')','#','@',':',';']
     word_haystack = self.downcase()
+    punctuation_list.each do | punctuation |
+      word_haystack.gsub!(punctuation,'')
+    end
     word_haystack = word_haystack.split(' ')
+
     # binding.pry
     return word_haystack.count(needle)
   end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -13,7 +13,11 @@ describe('String#word_count') do
   end
 
   it('handles strings with capitalizations within the search') do
-    expect(("The Rhododendron is the state flower of Washington. As far as a rhododendron goes, they're nice in a rhododendron way.").word_count('rhododendron')).to(eq(3))
+    expect(("The Rhododendron is the state flower of Washington. As far as a rhododendron goes, they're nice in a rhododendron way.").word_count('Rhododendron')).to(eq(3))
+  end
+
+  it('handles strings that abut punctuation characters') do
+    expect(('The Philippine national flower is called sampaguita. The sampaguita, like other perfume laden flowers, is highly sought after').word_count('sampaguita')).to(eq(2))
   end
 
 end
