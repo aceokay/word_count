2fad41706f69f61d21082f446f460e4c2a70747f
Austin Kincaid <aokincaid@gmail.com>
2015-08-07 12:42:50 -0700

    Refactor word_count method

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,17 +1,12 @@
 class String
   define_method(:word_count) do | needle |
     punctuation_list = [',','.','?','!','(',')','#','@',':',';']
-
     needle.downcase!()
-
     word_haystack = self.downcase()
-
-    punctuation_list.each do | punctuation |
+    punctuation_list.each() do | punctuation |
       word_haystack.gsub!(punctuation,'')
     end
-    
     word_haystack = word_haystack.split(' ')
-
     return word_haystack.count(needle)
   end
 end
